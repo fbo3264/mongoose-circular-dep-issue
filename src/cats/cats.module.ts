e2db3a016600aef3dd44d25f1cca6292c206b1b4
Friedrich Bösch <fboesch@gmx.net>
2020-12-11 12:42:43 +0100

    addimport schemas

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatsController } from './cats.controller';
-import { CatsService } from './cats.service';
-import { Cat, CatSchema } from './schemas/cat.schema';
+import {Module} from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose';
+import {CatsController} from './cats.controller';
+import {CatsService} from './cats.service';
+import {Cat, CatSchema} from './schemas/cat.schema';
+import {Owner, OwnerSchema} from "./schemas/owner.schema";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+    imports: [MongooseModule.forFeature([{name: Cat.name, schema: CatSchema}, {
+        name: Owner.name,
+        schema: OwnerSchema
+    }])],
     controllers: [CatsController],
     providers: [CatsService],
 })
-export class CatsModule {}
+export class CatsModule {
+}
